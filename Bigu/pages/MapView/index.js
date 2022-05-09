7947a0ae318ee8d5a0f9d315618cbd8bcddd2efb
italo1033 <italogammy1033@gmail.com>
2022-05-09 12:19:21 -0300

    Localização atual

--- Bigu/pages/MapView/index.js
+++ Bigu/pages/MapView/index.js
@@ -1,16 +1,37 @@
-import * as React from 'react';
+import React,{useState, useEffect} from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions ,PermissionsAndroid, Platform} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+
 
 export default function App() {
+  const [region, setRegion] = useState(null);
+  useEffect(()=>{
+    getMyLocation();
+  },[])
+  function getMyLocation(){
+    Geolocation.getCurrentPosition(info => {
+      info.coords.longitude
+      info.coords.latitude
+    });
+  }
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} initialRegion={{
-      latitude:  -9.406106,
-      longitude: -36.633143,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}/>
+      <MapView onMapReady={()=>{
+        Platform.OS === 'android'? PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+          .then(()=>{
+            console.log("Usuario Aceitou")
+          })
+        : ""
+      }} 
+      
+      style={styles.map} 
+      region={region}
+      zoomEnabled={true}
+      minZoomLevel={17}
+      showsUserLocatio={true}
+      loadingEnabled={true}/>
     </View>
   );
 }
